00cdff5cfa2121da50f3b4a9b90d0c530da822d8
Esper Kuijs <esper@fourdigits.nl>
2019-12-12 18:55:54 +0100

    Added intro for type safety

---
+++ python_type_demos/1_string_repeat.py
@@ -0,0 +1,9 @@
+def string_repeat(string: str, count: int) -> str:
+    """
+    Repeats a string N times
+    :param string: The string to repeat
+    :param count: The number of repeats
+    :return: The string repeated N times
+    """
+    return string * count
+string_repeat('hello', 15.6)
